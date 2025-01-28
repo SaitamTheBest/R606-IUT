497c526586684c27e088d676cde1423134f7bd3f
Matias DEVEZE <matiasdeveze@gmail.com>
2025-01-28 14:44:55 +0100

    fix: nullish coalescing should be preferred #25

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -7,6 +7,6 @@ export const setCookie = (name: string, value: string, days = 7) => {
   export const getCookie = (name: string): string | null => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+    if (parts.length === 2) return parts.pop()?.split(';').shift() ?? null;
     return null;
   };
